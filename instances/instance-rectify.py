ffa7a13dde2f56b5e673a11f437e9b7cfb39f307
dbrilhante <davisbrilhante@gmail.com>
2020-07-29 19:15:08 +0100

    correct instances

---
+++ instances/instance-rectify.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding : utf8 -*-
+
+import json
+import numpy as np
+import os
+
+topdir = 'out/'
+files = [f for f in os.listdir(topdir) if os.path.isfile(os.path.join(topdir, f))]
+#data = [[] for i in files]
+data = {}
+
+print('Loading files ', end='')
+
+for f in files:
+    print(f, end='')
+    with open(topdir+f, 'r') as infile:
+        try:
+            data = json.load(infile)
+        except:
+            continue
+    
+    for n,_ in enumerate(data['userEquipment']):
+        Lambda = 500 #milliseconds
+        newPackets = int(data['scenario']['simTime']/Lambda - 1)
+        arrival = np.random.poisson(500,newPackets).tolist()
+        data['userEquipment'][n]['nPackets'] = newPackets
+        data['userEquipment'][n]['packets'] = [sum(arrival[:i]) for i in range(newPackets)]
+
+    infile.close()
+
+    with open(topdir+f, 'w') as outfile:
+        json.dump(data,outfile,indent=4)
+    outfile.close()
