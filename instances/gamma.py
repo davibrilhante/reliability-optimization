a40e01d3f9e4bc3e9a510b5808bc24f517ba2a91
dbrilhante <davisbrilhante@gmail.com>
2021-01-08 13:15:39 +0000

    adated to run per instance

--- instances/gamma.py
+++ instances/gamma.py
@@ -33,72 +33,79 @@ def blockageScore(blockers, blockerList, ue, timeslot, opt=None):
 parser = ArgumentParser()
 parser.add_argument('-s','--speed')
 parser.add_argument('-b','--block')
+parser.add_argument('-x','--seed')
+
 #parser.add_argument('-i','--instance')
 
 args = parser.parse_args()
 
-seeds = [0] #[i for i in range(30)]
-
-for s in seeds:
+#seeds = [0] #[i for i in range(30)]
+#for s in seeds:
     #print('Actual seed: ', s)
-    filename = 'out/'+args.speed+'/'+args.block+'/'+str(s)
+
+
+filename = 'out/'+args.speed+'/'+args.block+'/'+str(args.seed)
+try:
     with open(filename,'r') as jsonfile:
         data = load(jsonfile)
 
     decompressor(data)
+except Exception as error:
+    print(error)
+    exit()
 
-    #print('Number of blockers: ', len(data['blockers']))
+#print('Number of blockers: ', len(data['blockers']))
 
-    # Trasform the obstacles in polygons
-    blockers = []
-    for obstacle in data['blockers']:
-        centroidX = obstacle['x']
-        centroidY = obstacle['y']
-        blockers.append(
-                Polygon(
-                [(centroidX - 0.5, centroidY + 0.5),
-                (centroidX + 0.5, centroidY + 0.5),
-                (centroidX + 0.5, centroidY - 0.5),
-                (centroidX - 0.5, centroidY - 0.5)]
-                        )
-                )
+# Trasform the obstacles in polygons
+blockers = []
+for obstacle in data['blockers']:
+    centroidX = obstacle['x']
+    centroidY = obstacle['y']
+    blockers.append(
+            Polygon(
+            [(centroidX - 0.5, centroidY + 0.5),
+            (centroidX + 0.5, centroidY + 0.5),
+            (centroidX + 0.5, centroidY - 0.5),
+            (centroidX - 0.5, centroidY - 0.5)]
+                    )
+            )
 
-    gamma = []
-    for m, bs in enumerate(data['baseStation']):
-        gamma.append([])
-        for n, ue in enumerate(data['userEquipment']):
-            gamma[m].append([])
-            listOfBlockers = []
-            for t in range(data['scenario']['simTime']):
+gamma = []
+for m, bs in enumerate(data['baseStation']):
+    gamma.append([])
+    for n, ue in enumerate(data['userEquipment']):
+        gamma[m].append([])
+        listOfBlockers = []
+        for t in range(data['scenario']['simTime']):
 
-                gamma[m][n].append(0)
+            gamma[m][n].append(0)
 
-                ueNewX = ue['position']['x'] + (ue['speed']['x']/3.6)*(t*1e-3)
-                ueNewY = ue['position']['y'] + (ue['speed']['y']/3.6)*(t*1e-3)
+            ueNewX = ue['position']['x'] + (ue['speed']['x']/3.6)*(t*1e-3)
+            ueNewY = ue['position']['y'] + (ue['speed']['y']/3.6)*(t*1e-3)
 
-                line = LineString([(bs['position']['x'], bs['position']['y']), (ueNewX, ueNewY)])
+            line = LineString([(bs['position']['x'], bs['position']['y']), (ueNewX, ueNewY)])
 
-                if data['blockage'][m][n][t] == 0:
-                    #Yep, there is a blockage but where?
-                    for b in blockers:
-                        if line.intersects(b):
-                            listOfBlockers.append(b) if b not in listOfBlockers else listOfBlockers
-                            #break
+            if data['blockage'][m][n][t] == 0:
+                #Yep, there is a blockage but where?
+                for b in blockers:
+                    if line.intersects(b):
+                        listOfBlockers.append(b) if b not in listOfBlockers else listOfBlockers
+                        #break
 
-                #gamma[m][n][t] = blockageScore(blockers, listOfBlockers, [ueNewX, ueNewY])
-                gamma[m][n][t] = blockageScore(blockers, listOfBlockers, ue, t, 2)
-                '''
-                if t%1000 == 0:
-                    print(t, gamma[m][n][t], ueNewX, ueNewY)
-                '''
-    y = dumps(gamma)
-    print(y)
-    '''
-    with open('gamma/'+args.speed+'/'+args.block+'/'+str(s), 'w') as out:
-        try:
-            dump(gamma, out)
-            out.close()
-        except Exception as e:
-            print(e)
-            exit()
-    '''
+            #gamma[m][n][t] = blockageScore(blockers, listOfBlockers, [ueNewX, ueNewY])
+            gamma[m][n][t] = blockageScore(blockers, listOfBlockers, ue, t, 2)
+            '''
+            if t%1000 == 0:
+                print(t, gamma[m][n][t], ueNewX, ueNewY)
+            '''
+y = dumps(gamma)
+print(y)
+'''
+with open('gamma/'+args.speed+'/'+args.block+'/'+str(s), 'w') as out:
+    try:
+        dump(gamma, out)
+        out.close()
+    except Exception as e:
+        print(e)
+        exit()
+'''
